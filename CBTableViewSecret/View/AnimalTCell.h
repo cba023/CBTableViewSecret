2d907ffbd293bf06e4271cda6a799dd79f087d47
cba023 <cba023@163.com>
2019-01-18 15:34:06 +0800

    modified

---
+++ CBTableViewSecret/View/AnimalTCell.h
@@ -0,0 +1,17 @@
+//
+//  AnimalTCell.h
+//  CBTableViewSecret
+//
+//  Created by flowerflower on 2019/1/18.
+//  Copyright © 2019 陈波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AnimalTCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CBTableViewSecret/View/AnimalTCell.m
@@ -0,0 +1,24 @@
+//
+//  AnimalTCell.m
+//  CBTableViewSecret
+//
+//  Created by flowerflower on 2019/1/18.
+//  Copyright © 2019 陈波. All rights reserved.
+//
+
+#import "AnimalTCell.h"
+
+@implementation AnimalTCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
